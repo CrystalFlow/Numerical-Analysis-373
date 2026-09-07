6ed9b744f20af96251af88536865e067b39d2fd1
Krishna Patel <223985069+CrystalFlow@users.noreply.github.com>
2026-09-07 13:39:25 -0400

    Working BisectionMethod.py implementation

--- BisectionMethod.py
+++ BisectionMethod.py
@@ -1,21 +1,16 @@
 import math
 # The bisection method to find roots of functions given a bracket. 
 
-
-#Input function for calculation before initializaiton. 
-def function(x):
-    return math.sin(x) - 0.5
-
-#Insert values that bracket a solution/root before initialization. 
-currentBracket = [0, 1]
-
 # Iterations needed for Bisection to find root, given a tolerance
-def iterationsNeeded (tolerance, currentBracket) -> int:
+def iterationsNeeded (tolerance, currentBracket):
     iteration = int( (math.log((currentBracket[1] - currentBracket[0]) / tolerance) / math.log(2) ) - 1 )
     #Might need to add to account for division issues
+    return iteration
 
-def bisectionProcess(currentBracket, tolerance):
+def bisectionProcess(tolerance, currentBracket):
     k = iterationsNeeded(tolerance, currentBracket)
+    print(f"Iterations needed: {k}")
+    print("Brackets: ")
     c = None
     for i in range(k):
         c = (currentBracket[1] + currentBracket[0]) / 2
@@ -23,4 +18,15 @@ def bisectionProcess(currentBracket, tolerance):
         if val > 0:   currentBracket[1] = c
         elif val < 0: currentBracket[0] = c
         else:         return c                  # Because the root is found
+        print(f"({currentBracket[0]}, {currentBracket[1]})")
     return c
+
+#Input function for calculation before initializaiton. 
+def function(x):
+    return math.sin(x) - 0.5
+
+#Insert values that bracket a solution/root before initialization. 
+currBracket = [0, 1]
+tol = 0.01
+ans = bisectionProcess(tol, currBracket)
+print(f"Root at x = {ans}, with tolerance of {tol}.")
